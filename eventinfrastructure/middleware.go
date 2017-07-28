2faa1c4faf4993432b055ee7f547ae7c2a9fad9e
Daniel Randall <dgrandall@me.com>
2017-07-28 14:18:19 -0600

    not perfect anymore:(

--- eventinfrastructure/middleware.go
+++ eventinfrastructure/middleware.go
@@ -18,11 +18,21 @@ import (
 )
 
 const ContextPublisher = "publisher"
+const ContextSubscriber = "subscriber"
 
 func BindPublisher(p *Publisher) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 			c.Set(ContextPublisher, p)
+			return next(c)
+		}
+	}
+}
+
+func BindSubscriber(s *Subscriber) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			c.Set(ContextSubscriber, s)
 			return next(c)
 		}
 	}
